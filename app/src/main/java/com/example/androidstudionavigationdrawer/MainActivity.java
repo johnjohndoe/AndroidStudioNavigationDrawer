415eae41025bdfc107f22641814b2b73ad054d94
Tobias Preuss <tobias.preuss@googlemail.com>
2015-11-29 16:30:06 +0100

    Import template from Android Studio 1.5.

--- app/src/main/java/com/example/androidstudionavigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/androidstudionavigationdrawer/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
